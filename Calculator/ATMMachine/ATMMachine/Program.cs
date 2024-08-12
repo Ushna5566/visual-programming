61e1ac1162018fe2f8de674b98fd69f40d098102
Ushna Hafeez <167672058+Ushna5566@users.noreply.github.com>
2024-08-11 23:02:37 -0700

    Add files via upload

---
+++ Calculator/ATMMachine/ATMMachine/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Account open");
+         
+            Console.WriteLine("Log in Account");
+            string Log = Console.ReadLine();
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Your Account Number");
+            string Account = Console.ReadLine();
+            Console.WriteLine("password");
+            int password = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Current balance");
+            string balance = Console.ReadLine();
+            Console.WriteLine("Deposit or Withdraw");
+            string depositorwithdraw  = Console.ReadLine();
+            Console.WriteLine("Enter Your Deposit Amount");
+            string Amount = Console.ReadLine();
+            Console.WriteLine("Total balance");
+            string Total = Console.ReadLine();
+           
+                Console.ReadLine();
+
+        }
+    }
+}
+
+
+    
+   
+
+
+
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{ 
+    class Solution
+    {
+        public double Subtraction(double a, double b)
+        {
+            double Result = a + b;
+            Console.WriteLine(Result);
+            return Result;
+        }
+        public double Addition(double a, double b)
+        {
+            double Result = a - b;
+            Console.WriteLine(Result);
+            return Result;
+        }
+        public double Multiplication(double a, double b)
+        {
+            double Result = a * b;
+            Console.WriteLine(Result);
+            return Result;
+        }
+        public double Division(double a, double b)
+        {
+            double Result = a / b;
+            Console.WriteLine(Result);
+            return Result;
+        }
+        public double Modulus(double a, double b)
+        {
+            double Result = a % b;
+            Console.WriteLine(Result);
+            return Result;
+        }
+    }
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter value one");
+            double a =Convert.ToDouble( Console.ReadLine());
+            Console.WriteLine("Please enter value two");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please Select opr -,+,*,/,%");
+            string opr = Console.ReadLine();
+           
+
+
+            Solution s = new Solution();
+            s.Addition(a ,b);
+            s.Subtraction(a ,b);
+            s.Multiplication(a, b);
+            s.Division(a, b);
+            s.Modulus(a, b);
+            Console.ReadLine();
+            if (opr =="-")
+            {
+                s.Subtraction(a, b);
+            }
+
+            else if (opr == "+")
+            {
+                s.Addition(a, b);
+
+            }
+            else if (opr == "*")
+            {
+                s.Multiplication(a, b);
+
+            }
+            else if (opr == "/")
+            {
+                s.Division(a, b);
+
+            }
+            else if (opr == "%")
+            {
+                s.Modulus(a, b);
+
+            }
+
+
+
+        }
+    }
+}
